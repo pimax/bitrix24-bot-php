f6997299c43bf22126d4b67536e826c1f0b0de95
pimax <m.pinyugin@gmail.com>
2016-04-20 00:38:00 +0300

    added Link

---
+++ Link.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace pimax\bitrix24;
+
+/**
+ * Class Link
+ *
+ * @package pimax\bitrix24
+ */
+class Link
+{
+    /**
+     * Dialog id
+     *
+     * @var int
+     */
+    protected $dialog_id = 0;
+
+    /**
+     * Link url
+     * @var string
+     */
+    protected $link = "";
+
+    /**
+     * Link title
+     *
+     * @var string
+     */
+    protected $title = "";
+
+    public function __construct($url, $title, $dialog_id = 0)
+    {
+        $this->dialog_id = $dialog_id;
+        $this->title = $title;
+        $this->url = $url;
+    }
+
+    /**
+     * Get message data
+     *
+     * @return array
+     */
+    public function getData()
+    {
+        $return = [];
+
+        if ($this->dialog_id) {
+            $return['DIALOG_ID'] = $this->dialog_id;
+        }
+
+        $return['LINK'] = [
+            'LINK' => $this->link,
+            'NAME' => $this->title
+        ];
+
+        return $return;
+    }
+}
